73c8be0f7417075442984f5f67eb2d48ed5ccf65
shsi20 <shis20@mcmaster.ca>
2024-07-08 00:29:48 -0400

    Implemented the customer order service: finish order, cancel order and pay

---
+++ src/main/java/com/imooc/converter/OrderMaster2OrderDTOConverter.java
@@ -0,0 +1,30 @@
+package com.imooc.converter;
+
+import com.imooc.dataobject.OrderMaster;
+import com.imooc.dto.OrderDTO;
+import org.springframework.beans.BeanUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Description: TODO
+ * @author: Shuting Shi
+ * @date: 2024年07月07日 8:24 a.m.
+ */
+public class OrderMaster2OrderDTOConverter {
+
+    public static OrderDTO convert(OrderMaster orderMaster) {
+        OrderDTO orderDTO = new OrderDTO();
+
+        BeanUtils.copyProperties(orderMaster, orderDTO);
+
+        return orderDTO;
+    }
+
+    public static List<OrderDTO> convert(List<OrderMaster> orderMasterList) {
+        return orderMasterList.stream().map(e ->
+                convert(e)
+        ).collect(Collectors.toList());
+    }
+}
