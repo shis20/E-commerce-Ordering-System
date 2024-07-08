eb5fda9f3e9390b83c6cd33906e0e6068272e671
shsi20 <shis20@mcmaster.ca>
2024-07-08 04:33:53 -0400

    Implemented the customer order service API (controller & service): finish order, cancel order

--- src/main/java/com/imooc/dto/OrderDTO.java
+++ src/main/java/com/imooc/dto/OrderDTO.java
@@ -1,8 +1,11 @@
 package com.imooc.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.imooc.dataobject.OrderDetail;
 import com.imooc.enums.OrderStatusEnum;
 import com.imooc.enums.PayStatusEnum;
+import com.imooc.utils.serializer.Date2LongSerializer;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -15,6 +18,7 @@ import java.util.List;
  * @date: 2024年07月07日 4:05 a.m.
  */
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderDTO {
 
     private String OrderId;
@@ -33,10 +37,13 @@ public class OrderDTO {
 
     private Integer payStatus = PayStatusEnum.WAIT.getCode();
 
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date CreateTime;
 
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date updateTime;
 
     List<OrderDetail> orderDetailList;
+
 
 }
---
+++ src/main/java/com/imooc/utils/serializer/Date2LongSerializer.java
@@ -0,0 +1,21 @@
+package com.imooc.utils.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * @Description: TODO
+ * @author: Shuting Shi
+ * @date: 2024年07月08日 3:45 a.m.
+ */
+public class Date2LongSerializer extends JsonSerializer<Date> {
+    @Override
+    public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeNumber(date.getTime() / 1000);
+    }
+}
